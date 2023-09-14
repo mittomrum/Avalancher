e8a5b2706f4208a339937b424fd4ba1820aa9c41
Tomrum Fredrik <mittomrum@gmail.com>
2023-09-14 18:48:59 +0200

    Crash and FinishLine Msg using textmeshpro

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,20 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class CrashDetector : MonoBehaviour
-{ 
-    //This script it attached to the level, it should detect and handle the crash of the player, if the player is crashing with his circle collider trigger, I would like to print out in the debug log "Crash" and then restart the level.
-    //I would like to use the OnTriggerEnter2D function to detect the crash, and then use the SceneManager to restart the level.
+{
+    // If the player is touching the tag ground with his trigger collider, it should print out crash
+    // and after 2 seconds restart the level.
 
+    // Inspector variables
+    [SerializeField] private string tagToCheck = "Ground";
+    [SerializeField] private float secondsToWait = 1f;
 
- // the collider is a circle collider
+    // Turn on game object when crash detected
+    public TextMeshProUGUI textToDisplay;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == tagToCheck)
         {
             Debug.Log("Crash");
-            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            ShowCrashMessage("Crash");
+            StartCoroutine(HideCrashMessage(secondsToWait));
+            StartCoroutine(RestartLevel(secondsToWait));
         }
+    }
+
+    // Enable the TextMeshPro object with a crash message
+    void ShowCrashMessage(string message)
+    {
+        textToDisplay.text = message; // Corrected from crashText to textToDisplay
+        textToDisplay.gameObject.SetActive(true); // Corrected from crashText to textToDisplay
+    }
+
+    // Disable the TextMeshPro object after a delay
+    IEnumerator HideCrashMessage(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        textToDisplay.gameObject.SetActive(false); // Corrected from crashText to textToDisplay
+    }
+
+    IEnumerator RestartLevel(float secondsToWait = 1)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
     }
 }
--- Assets/Scripts/FinishingLine.cs
+++ Assets/Scripts/FinishingLine.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FinishingLine : MonoBehaviour
 {
-    // If the player is colliding with the finishing line, I would like to print out in the debug log "Finish" and then restart the level.
+    // If the player is touching the tag 'Finish' with his trigger collider, it should print out that the player has finished the level
+    // and after 2 seconds restart the level.
+
+    // Inspector variables
+    [SerializeField] private string tagToCheck = "Finish";
+    [SerializeField] private float secondsToWait = 1f;
+
+    public TextMeshProUGUI textToDisplay;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.tag == tagToCheck)
         {
-            Debug.Log("Finish");
-            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+            ShowCrashMessage("You have finished the level!");
+            StartCoroutine(HideCrashMessage(secondsToWait));
+            StartCoroutine(RestartLevel(secondsToWait));
         }
     }
-}
 
+    // Enable the TextMeshPro object with a crash message
+    void ShowCrashMessage(string message)
+    {
+        textToDisplay.text = message; // Corrected from crashText to textToDisplay
+        textToDisplay.gameObject.SetActive(true); // Corrected from crashText to textToDisplay
+    }
+
+    // Disable the TextMeshPro object after a delay
+    IEnumerator HideCrashMessage(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        textToDisplay.gameObject.SetActive(false); // Corrected from crashText to textToDisplay
+    }
+
+    IEnumerator RestartLevel(float secondsToWait = 1)
+    {
+        yield return new WaitForSeconds(secondsToWait);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name);
+    }
+}
